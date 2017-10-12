d566e50035ac434fe7cf1f69422e2ff371858372
Pragistyo <new.SOS.hope@outlook.com>
2017-10-12 16:57:33 +0700

    masih setting di frontend, blm muncul data

--- client-hacktivpress/src/components/Home.vue
+++ client-hacktivpress/src/components/Home.vue
@@ -9,10 +9,10 @@
 
    <div class="form horizontal">
      <h2>Login</h2>
-     <form class="" method="post">
+     <form class="" method="post" @submit.prevent="">
        <fieldset>
-         <input class="form-control" type="text" name="username" v-model='loginForm.username' placeholder="username"><br>
-         <input class="form-control"type="text" name="password" v-model='loginForm.password' placeholder="password"><br>
+         <input class="form-control" type="text" name="username" v-model='usernamelogin' placeholder="username"><br>
+         <input class="form-control"type="text" name="password" v-model='passwordlogin' placeholder="password"><br>
          <input class="form-control"type="button" value="submit" v-on:click.prevent="login">
        </fieldset>
      </form>
@@ -23,23 +23,73 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'Home',
   data () {
     return {
       msg: 'Welcome to Trainerpress',
+      username: '',
       article: [],
-      loginForm: {
-        username: '',
-        password: ''
-      },
+      usernamelogin: null,
+      passwordlogin: null,
       postForm: {
-        title: '',
-        category: '',
+        title: null,
+        category: null,
         author: null,
-        content: ''
-      },
-      user: ''
+        content: null
+      }
+    }
+  },
+  mounted: function () {
+    this.getData()
+  },
+  methods: {
+    getData () {
+      var self = this
+      axios.get(`http://localhost:3000/blog`)
+      .then(result => {
+        self.articles = result.data
+      })
+    },
+    postData () {
+      var self = this
+      axios.post(`http://localhost:3000/blog`, self.postForm)
+      .then(response => {
+        this.getData()
+        self.postForm = {
+          title: null,
+          category: null,
+          author: null,
+          content: null
+        }
+      })
+    },
+    getCategory (category) {
+      var self = this
+      axios.get(`http://localhost:3000/blog/category/${category}`)
+      .then(result => {
+        self.article = result
+      })
+    },
+    login () {
+      // var self = this
+      alert('masuk login')
+      alert(this.username)
+      alert(this.password)
+      axios.post(`http://localhost:3000/users/login`, {
+        username: 'denny',
+        password: '1234'
+      })
+      .then(result => {
+        alert('token :' + result)
+      //   // this.getData()
+      //   self.loginForm.username = null
+      //   self.loginForm.password = null
+      })
+    },
+    register () {
+
     }
   }
 }
